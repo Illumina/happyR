57723e34cfd19d48a3ad608921233f10e0511883
Ben Moore <bmoore1@illumina.com>
2017-02-23 12:11:46 +0000

    fix demo data

--- inst/bash/run_happy.sh
+++ inst/bash/run_happy.sh
@@ -3,6 +3,6 @@
 # point to hap.py install
 HAP=/illumina/development/haplocompare/haplocompare-v0.3.6/bin/hap.py
 
-$HAP ../extdata/PG_NA12878_chr21.vcf.gz ../extdata/NA12878-GATK3-chr21.vcf.gz \
+$HAP ../extdata/PG_NA12878_chr21.vcf.gz ../extdata/NA12878_chr21.vcf.gz \
      -f ../extdata/PG_Conf_chr21.bed.gz --roc QUAL --roc-filter LowQual \
      -o happy_demo --no-json
