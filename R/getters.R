d2a9c9556c1165f62f6cc76c55bf92c3537a3e55
Ben Moore <bmoore1@illumina.com>
2017-03-15 17:12:32 +0000

    edit extract fn; add tests; only attach dplyr

--- R/getters.R
+++ R/getters.R
@@ -1,5 +1,4 @@
 ## Cleaner access to hap.py data
-
 pr_data <- function(happy_result, filter = c("ALL", "PASS", "SEL"),
                     start_from = c("ALL", "PASS"),
                     selectively_filter = c(TRUE, FALSE),
@@ -20,4 +19,47 @@ pr_data <- function(happy_result, filter = c("ALL", "PASS", "SEL"),
     # filter by subset, warn if fails
   }
 
+}
+
+
+#' Extract tables from hap.py result lists
+#'
+#' Extract tables from multiple hap.py result objects and combine
+#' into a single \code{data.frame}. Source information from each
+#' result is added as an additional column (\code{from}).
+#'
+#' @param happy_result_list A \code{happy_result_list} object, created
+#'   by combining \code{happy_result}s together with \code{c}
+#' @param table Table of data to extract from each result
+#'
+#'
+#' @export
+extract <- function(happy_result_list, item = c("summary", "extended")) {
+  # validate input
+  if (!"happy_result_list" %in% class(happy_result_list)) {
+    stop("Must provide a happy_result_list object.")
+  }
+
+  item <- match.arg(item)
+
+  # extract results into a data.frame
+  item_list <- lapply(happy_result_list, function(d) {
+    if (!item %in% names(d)) {
+      stop("Could not find ", item, " in happy_result_list")
+    }
+    table <- d[[item]]
+    table$from <- attr(d, "from")
+    table
+  })
+  df <- dplyr::bind_rows(item_list)
+
+  # set class
+  if (item == "summary") {
+    class(df) <- c("happy_summary", class(df))
+  }
+  if (item == "extended") {
+    class(df) <- c("happy_extended", class(df))
+  }
+
+  return(df)
 }
--- tests/testthat/test_input.R
+++ tests/testthat/test_input.R
@@ -13,33 +13,3 @@ test_that("read_happy reads hap.py results to an R data structure", {
   expect_equal(length(results$pr_curve), 7)
 })
 
-
-context("Combining results")
-
-test_that("happy results can be combined into a single object", {
-  results2 <- results
-  expect_warning(
-    results_list <- c(results, results2)
-    )
-  expect_true(inherits(results_list, "happy_result_list"))
-  expect_equal(length(results_list), 2)
-})
-
-test_that("happy_result_list can be extended with a new happy_result", {
-  results2 <- results
-  expect_warning(results_list <- c(results, results2))
-  results3 <- results
-
-  expect_warning(results_list <- c(results_list, results3))
-  expect_true(inherits(results_list, "happy_result_list"))
-  expect_equal(length(results_list), 3)
-})
-
-test_that("combining happy_results warns of redundancy", {
-  results2 <- results
-  expect_warning(results_list <- c(results, results2))
-
-  results3 <- results
-  attr(results3, "from") <- "dummy/path/to/happy"
-  expect_silent(rl <- c(results2, results3))
-})
