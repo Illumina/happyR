2948b998fcb9809d323eefa1d16d62caba108edc
Ben Moore <bmoore1@illumina.com>
2017-06-22 11:16:05 +0100

    v0.3.9 eg data with runinfo

--- inst/bash/run_happy.sh
+++ inst/bash/run_happy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # point to hap.py install
-HAP=/illumina/development/haplocompare/haplocompare-v0.3.6/bin/hap.py
+HAP=${1?param missing - /path/to/hap.py.}
 
 $HAP ../extdata/PG_NA12878_chr21.vcf.gz ../extdata/NA12878_chr21.vcf.gz \
      -f ../extdata/PG_Conf_chr21.bed.gz --roc QUAL --roc-filter LowQual \
